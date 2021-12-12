f4a17813113a6e2eb2827280764a603401b26f79
AprilH <89118804+AprilLH@users.noreply.github.com>
2021-12-11 17:35:27 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## There are two functions makeCacheMatrix and cacheSolve
+
 ## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -22,7 +25,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 
